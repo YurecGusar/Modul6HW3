b998c9cd12b6524ffb7a10e5e6525c9bcce7f855
DESKTOP-HO5O0U5\leoon <leoonovn@gmail.com>
2021-11-02 16:32:30 +0200

    Создал атрибут

---
+++ Modul6HW3/Modul6HW3/Attributes/ExtraHeaderAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modul6HW3.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class ExtraHeaderAttribute : Attribute
+    {
+        public ExtraHeaderAttribute(string kay, string value)
+        {
+
+        }
+
+        public string Kay { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Modul6HW3/Modul6HW3/Middlewares/MyMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Modul6HW3.Middlewares
+{
+    public class MyMiddleware
+    {
+        private RequestDelegate _next;
+
+        public MyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next.Invoke(context);
+        }
+    }
+}
